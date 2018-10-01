3ec9b83d5ff9ba66e363d478727ed61f6cf391eb
Mihail Mikov <mihail.mikov@skyscanner.net>
2018-10-01 13:21:03 +0300

    better menu

--- the_code/src/common/menu.js
+++ the_code/src/common/menu.js
@@ -15,9 +15,11 @@ const Menu = class extends React.Component {
   constructor() {
     super();
 
+    this.menuRef = React.createRef();
+
     this.state = { opened: false };
 
-    this.toggle = (event) => {
+    this.followLink = (event) => {
       const eventTargetIsLink = event.target.tagName === 'A';
       const eventTargetIsTitle = event.target.className.includes('course_title');
       this.setState(({ opened }) => {
@@ -25,13 +27,18 @@ const Menu = class extends React.Component {
           window.location += '#/0';
           return { opened: false };
         };
-
-        return { opened: !opened };
-      })
+      });
     };
+    
+    this.toggle = (e) => {
+      e.preventDefault();
+      this.setState(({opened}) => ({ opened: !opened }));
+    }
 
-    this.hide = () => {
-      this.setState({ opened: false });
+    this.hide = (e) => {
+      if (this.menuRef.current && !this.menuRef.current.contains(e.target)) {
+        this.setState({ opened: false });
+      }
     }
   }
 
@@ -49,9 +56,9 @@ const Menu = class extends React.Component {
         <li key={module.menu.path}><RouterLink activeClassName="menu_active_module" exact to={module.menu.path}>{module.menu.title}</RouterLink></li>
       )}
     </ul>);
-    return <div onClick={this.toggle} class="course_title">
-      <span style={{'font-size': '0.75em'}}>⬇️ COS310: Intro to Git > <Switch>{[...courseModules].reverse().map(
-        module => <Route path={module.menu.path} render={() => module.menu.title} />)}</Switch></span>
+    return <div ref={this.menuRef} onClick={this.followLink} class="course_title">
+      <a href="#" onClick={this.toggle}><span className="git-logo"><img width="32px" src="images/git-logo.png" /> COS310: Intro to Git &gt; <Switch>{[...courseModules].reverse().map(
+        module => <Route path={module.menu.path} render={() => module.menu.title} />)}</Switch></span></a>
       {this.state.opened && menu}
     </div>
   }
